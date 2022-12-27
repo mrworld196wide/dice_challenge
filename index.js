8ae2008f14c5bbedba6da33c9d39d4f00da5c0de
mrworld196wide <shubhum19@gmail.com>
2022-12-27 17:10:18 +0530

    dice image synced with randomnumbers

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ let randomNumber1= Math.floor(Math.random()*6)+1;
 let randomNumber2=Math.floor(Math.random()*6)+1;
 // console.log(randomNumber1);
 // console.log(randomNumber2);
+// document.querySelector("div div img").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+let i2=document.getElementById("i2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
