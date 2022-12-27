dc919c7c9dbc8998c7d1b36c98157285b50a0bd7
mrworld196wide <shubhum19@gmail.com>
2022-12-27 17:15:20 +0530

    heading is changed according to the winner

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ let randomNumber2=Math.floor(Math.random()*6)+1;
 // console.log(randomNumber1);
 // console.log(randomNumber2);
 // document.querySelector("div div img").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-let i2=document.getElementById("i2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+document.getElementById("i1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.getElementById("i2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+if(randomNumber1>randomNumber2){
+    document.getElementById("res").innerHTML = "🚩Player 1 Wins"
+}else{
+    document.getElementById("res").innerHTML = "Player 2 Wins🚩"
+}
